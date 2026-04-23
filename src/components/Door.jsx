58235dbbeb4df708a883e7308671b41a871650b1
Farzin Ahmadi <farzin.ahmadi@hotmail.com>
2026-04-23 19:14:22 -0300

    remove alt from doors

--- src/components/Door.jsx
+++ src/components/Door.jsx
@@ -40,7 +40,7 @@ function Door({ door, onDrop, selectedKeyId, isGenPhase }) {
       }}
     >
       <div className="door-visual">
-        <img src={doorAssetPath} alt={`${door.color} door`} className="door-image" />
+        <img src={doorAssetPath} className="door-image" />
         {feedback === 'correct' && <div className="feedback-icon correct">✔️</div>}
         {feedback === 'incorrect' && <div className="feedback-icon incorrect">❌</div>}
         {feedback === 'neutral' && <div className="feedback-icon neutral">👍</div>}
